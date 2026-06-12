ee076538a9927f0846a568e5f05af4211fb64eba
Michael <193541888+Bobsled6@users.noreply.github.com>
2026-06-12 15:44:10 -0400

    added function to check for win condition, filter to check if the board is full

--- javascript.js
+++ javascript.js
@@ -17,8 +17,8 @@ function Gameboard() {
         let value = 0;
         return value;
     }
-    console.log(board)
     return board;
+    
 }
 
 
@@ -38,24 +38,53 @@ function gameflow() {
 
      const player1Marker = players()[0].marker;
      const player2Marker = players()[1].marker;
+     let currentBoard = Gameboard();
       
     function markCell (row,column,player) {
+      
       const player1Marker = players()[0].marker;
       const player2Marker = players()[1].marker;
       let playerMarker;
+
       if (player === 1){playerMarker = player1Marker}
       else if(player === 2){playerMarker = player2Marker}
-      
-      let cellValue = playerMarker;
-      Gameboard()[row][column] = cellValue;
-    
+      else (console.log("Error, no player selected"))
+      currentBoard[row][column] = playerMarker;
+      return currentBoard;
     }
-    markCell(0,2,2)
+    let unmarkedCells = 0;
+    
+    function filterUnmarked(cell) {
+        return !(cell === 0)
+    }
 
+    function winCheck() {
+        let board = currentBoard;
+    if(
+        (board[0][0] === board[0][1] && board[0][0] === board[0][2] && !(board[0][0] === 0))
+    ||  (board[1][0] === board[1][1] && board[1][0] === board[1][2] && !(board[1][0] === 0))
+    ||  (board[2][0] === board[2][1] && board[2][0] === board[2][2] && !(board[2][0] === 0))
+    ||  (board[0][0] === board[1][0] && board[0][0] === board[2][0] && !(board[0][0] === 0))
+    ||  (board[0][1] === board[1][1] && board[0][1] === board[2][1] && !(board[0][1] === 0))
+    ||  (board[0][2] === board[1][2] && board[0][2] === board[2][2] && !(board[0][2] === 0))
+    ||  (board[0][0] === board[1][1] && board[0][0] === board[2][2] && !(board[0][0] === 0))
+    ||  (board[0][2] === board[1][1] && board[0][2] === board[2][0] && !(board[0][2] === 0))) {
+            console.log("success")
+        }
+
+    }
+
+winCheck();
+    
+    const filteredBoard = currentBoard.filter(filterUnmarked);
+    if (filteredBoard.length === 0) {
+        console.log("tie")
+    }
 };
-
 
 
 
 players();
 gameflow();
+
+
